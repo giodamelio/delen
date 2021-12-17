47329c4ca15fcb53121a4e2a8d06a7723f027ac0
Gio d'Amelio <giodamelio@gmail.com>
2021-12-17 18:26:45 +0000

    Successfuly upload a file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,9 +17,29 @@ func main() {
 	// Load the templates
 	r.LoadHTMLGlob("templates/*")
 
+	// Setup API
+	ApiRouter(r.Group("/api"))
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
 	r.Run()
+}
+
+func ApiRouter(r *gin.RouterGroup) {
+	r.POST("/upload", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+		files := form.File["files[]"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!\n", len(files)))
+	})
 }
