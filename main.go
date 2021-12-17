07272fb5f00d9ce9ef05b2768a444508176d9c2e
Gio d'Amelio <giodamelio@gmail.com>
2021-12-17 17:45:19 +0000

    Serve a basic html template

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
+	// Trust no proxies
+	r.SetTrustedProxies(nil)
+
+	// Load the templates
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World!")
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()
 }
