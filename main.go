c1d23adf915af8e01916af40801801ec8eaf0a60
Gio d'Amelio <giodamelio@gmail.com>
2021-12-25 01:29:16 +0000

    Do basic content negotiation

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/elnormous/contenttype"
 	"github.com/gin-gonic/gin"
 	"github.com/giodamelio/delen/db"
 	_ "github.com/lib/pq"
@@ -22,6 +23,22 @@ func setupDb() (*db.Queries, error) {
 	return db.New(dbConn), nil
 }
 
+// Add the Accepted header to the request context
+func AcceptHeader() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		availableMediaTypes := []contenttype.MediaType{
+			contenttype.NewMediaType("text/html"),
+			contenttype.NewMediaType("application/json"),
+		}
+
+		accepted, _, _ := contenttype.GetAcceptableMediaType(c.Request, availableMediaTypes)
+		log.Println("Accepted media type:", accepted.String())
+		c.Set("accept", accepted.String())
+
+		c.Next()
+	}
+}
+
 func main() {
 	queries, err := setupDb()
 	if err != nil {
@@ -33,11 +50,19 @@ func main() {
 	// Trust no proxies
 	r.SetTrustedProxies(nil)
 
+	// Handle the accept header
+	r.Use(AcceptHeader())
+
 	// Load the templates
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		accept := c.GetString("accept")
+		if accept == "application/json" {
+			c.JSON(http.StatusOK, gin.H{"todo": "Not implemented in JSON"})
+		} else {
+			c.HTML(http.StatusOK, "index.html", gin.H{})
+		}
 	})
 
 	r.POST("/upload", func(c *gin.Context) {
@@ -71,7 +96,12 @@ func main() {
 
 		log.Printf("%d files uploaded\n", len(files))
 
-		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded\n", len(files)))
+		accept := c.GetString("accept")
+		if accept == "application/json" {
+			c.JSON(http.StatusOK, gin.H{"todo": "Not implemented in JSON"})
+		} else {
+			c.String(http.StatusOK, fmt.Sprintf("%d files uploaded\n", len(files)))
+		}
 	})
 
 	r.Run()
