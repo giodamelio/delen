887ba94a150efe315920c121f03970ac5074f238
Gio d'Amelio <giodamelio@gmail.com>
2021-12-26 19:45:07 +0000

    Chi hello world

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	log.Println("Hello World!")
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+		w.Write([]byte("pong"))
+	})
+
+	http.ListenAndServe(":8080", r)
 }
