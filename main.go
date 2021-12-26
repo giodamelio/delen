10a677c3f32ee5e8655b01c3b6e73499527f2bd8
Gio d'Amelio <giodamelio@gmail.com>
2021-12-26 21:03:26 +0000

    Replace log with Zerolog

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type formatKey struct{}
@@ -19,13 +21,13 @@ func formatDecider(next http.Handler) http.Handler {
 
 		// If the format already exists, bail out
 		if exists {
-			log.Println("format already exists:", format)
+			log.Trace().Msgf("format already exists: %s", format)
 			next.ServeHTTP(w, r)
 			return
 		}
 
 		// If the format does not exist, set it to text/html by default
-		log.Println("setting format as:", "text/html")
+		log.Trace().Msgf("setting format as: %s", "text/html")
 		ctx = context.WithValue(ctx, formatKey{}, "text/html")
 
 		next.ServeHTTP(w, r.WithContext(ctx))
@@ -33,6 +35,8 @@ func formatDecider(next http.Handler) http.Handler {
 }
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -42,7 +46,8 @@ func main() {
 		ctx := r.Context()
 		format := ctx.Value(formatKey{}).(string)
 
-		log.Println("Format:", format)
+		log.Print("Format:", format)
+
 		w.Write([]byte(fmt.Sprintln("PONG")))
 		w.Write([]byte(fmt.Sprintln("format:", format)))
 	})
@@ -76,7 +81,7 @@ func overwriteFormatHandler(format string) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
-			log.Println("overridding format via path extension:", format)
+			log.Trace().Msgf("overridding format via path extension: %s", format)
 			ctx = context.WithValue(ctx, formatKey{}, format)
 			h.ServeHTTP(w, r.WithContext(ctx))
 		})
@@ -85,9 +90,9 @@ func overwriteFormatHandler(format string) func(http.Handler) http.Handler {
 
 // Log out routes
 func logRoutes(r *chi.Mux) {
-	log.Println("Registered routes:")
+	log.Warn().Msg("Registered routes:")
 	chi.Walk(r, func(method, route string, _ http.Handler, _ ...func(http.Handler) http.Handler) error {
-		log.Println(" ", method, route)
+		log.Warn().Msgf(" %s %s", method, route)
 		return nil
 	})
 }
