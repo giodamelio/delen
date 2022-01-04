d659c912ef1103a2da85eb20437bc323597cfcd0
Gio d'Amelio <giodamelio@gmail.com>
2022-01-04 22:38:11 +0000

    Move Json Extension Rewrite fairing into it's own module

---
+++ src/fairings/mod.rs
@@ -0,0 +1,38 @@
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Accept,
+    Data, Request,
+};
+
+pub struct JSONExtensionRewrite;
+
+#[rocket::async_trait]
+impl Fairing for JSONExtensionRewrite {
+    fn info(&self) -> Info {
+        Info {
+            name: "JSON Extension Rewriter",
+            kind: Kind::Request,
+        }
+    }
+
+    async fn on_request(&self, request: &mut Request<'_>, _data: &mut Data<'_>) {
+        let origin = request.uri().clone();
+
+        // If the path ends with a file extension, remove it and add a matching Accept header
+        // If the mapping breaks, keep the original path
+        let new_origin = if origin.path().ends_with(".json") {
+            // Add accept header
+            request.replace_header(Accept::JSON);
+
+            // Remove the extension from the path
+            // It is safe to unwrap the `strip_suffix` because we already checked it's existance
+            origin
+                .map_path(|path| path.strip_suffix(".json").unwrap())
+                .unwrap_or_else(|| origin.to_owned())
+        } else {
+            origin.to_owned()
+        };
+
+        request.set_uri(new_origin);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
-use rocket::fairing::AdHoc;
-use rocket::http::Accept;
 use rocket::serde::json::Json;
 use rocket::serde::Serialize;
 use rocket::tokio::time::{sleep, Duration};
 use rocket::{get, launch, routes};
 use rocket_dyn_templates::Template;
+
+mod fairings;
 
 #[derive(Serialize, Debug)]
 #[serde(crate = "rocket::serde")]
@@ -45,29 +45,5 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index_json, index_html, hello, delay])
         .attach(Template::fairing())
-        .attach(AdHoc::on_request(
-            "ExtensionOverride",
-            |request, _response| {
-                Box::pin(async move {
-                    let origin = request.uri().clone();
-
-                    // If the path ends with a file extension, remove it and add a matching Accept header
-                    // If the mapping breaks, keep the original path
-                    let new_origin = if origin.path().ends_with(".json") {
-                        // Add accept header
-                        request.replace_header(Accept::JSON);
-
-                        // Remove the extension from the path
-                        // It is safe to unwrap the `strip_suffix` because we already checked it's existance
-                        origin
-                            .map_path(|path| path.strip_suffix(".json").unwrap())
-                            .unwrap_or_else(|| origin.to_owned())
-                    } else {
-                        origin.to_owned()
-                    };
-
-                    request.set_uri(new_origin);
-                })
-            },
-        ))
+        .attach(fairings::JSONExtensionRewrite)
 }
