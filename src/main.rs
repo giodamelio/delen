06aac5271f145a0a75c32e28a3b7e993fa74dd41
Gio d'Amelio <giodamelio@gmail.com>
2022-01-04 15:42:43 +0000

    We can Async

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#[macro_use]
-extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
+use rocket::{get, launch, routes};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -11,7 +11,13 @@ fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, hello])
+    rocket::build().mount("/", routes![index, hello, delay])
 }
