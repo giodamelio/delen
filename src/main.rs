a9c98bce5289d0aec1d8233296ca48c15a426ea6
Gio d'Amelio <giodamelio@gmail.com>
2022-01-05 17:54:08 +0000

    Launch Rocket with an external Tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rocket::http::Accept;
 use rocket::serde::json::Json;
 use rocket::serde::Serialize;
 use rocket::tokio::time::{sleep, Duration};
-use rocket::{get, launch, routes};
+use rocket::{get, routes};
 use rocket_dyn_templates::Template;
 
 mod fairings;
@@ -41,11 +41,24 @@ async fn delay(seconds: u64) -> String {
     format!("Waited for {} seconds", seconds)
 }
 
-#[launch]
-fn rocket() -> _ {
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build()
+//         .mount("/", routes![index_json, index_html, hello, delay])
+//         .attach(Template::fairing())
+//         .attach(fairings::ExtensionRewrite::new(".json", Accept::JSON))
+//         .attach(fairings::ExtensionRewrite::new(".html", Accept::HTML))
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), rocket::Error> {
     rocket::build()
         .mount("/", routes![index_json, index_html, hello, delay])
         .attach(Template::fairing())
         .attach(fairings::ExtensionRewrite::new(".json", Accept::JSON))
         .attach(fairings::ExtensionRewrite::new(".html", Accept::HTML))
+        .ignite()
+        .await?
+        .launch()
+        .await
 }
