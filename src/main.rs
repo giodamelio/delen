36b4588aa1b3aa9443966c9d2c6f20cbe1c7ecff
Gio d'Amelio <giodamelio@gmail.com>
2022-01-06 17:05:53 +0000

    Move error into its own module

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use rocket::{get, routes};
 use rocket_dyn_templates::Template;
 use sqlx::{migrate::Migrator, sqlite, Pool, Sqlite};
 
+use error::Result;
+
+mod error;
 mod fairings;
 
 #[derive(Serialize, Debug)]
@@ -75,26 +78,3 @@ async fn main() -> Result<()> {
 
     Ok(())
 }
-
-// Below is a hack to make Rocket and Anyhow play nice
-// Stolen from https://crates.io/crates/rocket_anyhow
-pub type Result<T = ()> = std::result::Result<T, Error>;
-
-#[derive(Debug)]
-pub struct Error(pub anyhow::Error);
-
-impl<E> From<E> for crate::Error
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(error: E) -> Self {
-        Error(error.into())
-    }
-}
-
-impl<'r, 'o: 'r> rocket::response::Responder<'r, 'o> for Error {
-    fn respond_to(self, request: &rocket::Request<'_>) -> rocket::response::Result<'o> {
-        rocket::response::Debug(self.0).respond_to(request)
-    }
-}
-// </hack>
