3002dc9f40b939c2245643491729acab3c3d037e
Gio d'Amelio <giodamelio@gmail.com>
2022-03-26 22:27:15 +0000

    Wipe clean for a fresh rewrite

--- migrations/20220105172347_create-item.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS item (
-  id          INTEGER PRIMARY KEY,
-  filename    TEXT NOT NULL,
-  contents    BLOB NOT NULL,
-  filetype    TEXT,
-  created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
