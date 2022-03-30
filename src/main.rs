9252cd4008c38086943404f93f60cad0eaae2773
Gio d'Amelio <giodamelio@gmail.com>
2022-03-30 21:30:57 +0000

    Use Template and Guards to send both HTML and JSON

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::serde::{json::Json, Serialize};
+use rocket_dyn_templates::Template;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Index {
+    message: String,
+}
+
+#[get("/", format = "html")]
+fn index_html() -> Template {
+    let context = Index {
+        message: "Hello World!".into(),
+    };
+
+    Template::render("index", context)
+}
+
+#[get("/", format = "json", rank = 2)]
+fn index_json() -> Json<Index> {
+    let context = Index {
+        message: "Hello World!".into(),
+    };
+
+    Json(context)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(Template::fairing())
+        .mount("/", routes![index_html, index_json])
 }
